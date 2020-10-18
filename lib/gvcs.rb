50bcf30baef24a17e8e7eebafb35c2b2aef7e6c0
Chris Liaw <chrisliaw@antrapol.com>
2020-10-19 00:18:09 +0800

    Initial\ checkin

---
+++ lib/gvcs.rb
@@ -0,0 +1,17 @@
+require "gvcs/version"
+
+module Gvcs
+  class Error < StandardError; end
+  # Your code goes here...
+
+  class Vcs
+  end
+
+  class Workspace
+  end
+
+  class Repository
+
+  end
+
+end
---
+++ lib/gvcs/version.rb
@@ -0,0 +1,3 @@
+module Gvcs
+  VERSION = "0.1.0"
+end
